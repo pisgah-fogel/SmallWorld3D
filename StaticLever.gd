ff565d5ae40d6bdd155c514095b2fe9455f23ade
DETREZ Aubin <aubin.detrez@groupe-esigelec.org>
2020-09-02 21:14:43 +0200

    Add Lever's mesh + correct collision shapes

--- StaticLever.gd
+++ StaticLever.gd
@@ -1,15 +1,22 @@
 extends StaticBody
 
+onready var mAnimationPlayer = $Lever/AnimationPlayer
+
 var active = false
 export(String, FILE, "*.shader") var shaderPath = "res://GrayScale.shader"
+
+func _ready():
+	mAnimationPlayer.play("Off")
 
 func _on_Area_body_entered(body):
 	if get_tree().get_root().get_node("TestScene").getActiveShaderFilename() != shaderPath:
 		get_tree().get_root().get_node("TestScene").enableScreenShader(shaderPath)
 		active = true
 		print("Set shader active")
+		mAnimationPlayer.play("On")
 	else:
 		get_tree().get_root().get_node("TestScene").enableScreenShader(null)
 		active = false
 		print("Set shader inactive")
+		mAnimationPlayer.play("Off")
 		
